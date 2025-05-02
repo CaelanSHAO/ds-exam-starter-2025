ac0fed88166c10b8755081b046530e97018dee7a
JIAYI SHAO <sjy08015342@163.com>
2025-05-02 09:56:05 +0100

    Added new endpoint to API

--- lambdas/question1.ts
+++ lambdas/question1.ts
@@ -1,13 +1,48 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = createDDbDocClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
   try {
     console.log("Event: ", JSON.stringify(event));
+
+    // Check if this is the crew/movies endpoint
+    if (event.pathParameters?.movieId && event.queryStringParameters?.role) {
+      const movieId = event.pathParameters.movieId;
+      const role = event.queryStringParameters.role;
+
+      // Query DynamoDB for the crew member
+      const commandOutput = await client.send(
+        new GetCommand({
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            movieId: movieId,
+            role: role
+          }
+        })
+      );
+
+      if (!commandOutput.Item) {
+        return {
+          statusCode: 404,
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({ message: "Crew member not found" }),
+        };
+      }
+
+      return {
+        statusCode: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(commandOutput.Item),
+      };
+    }
 
     return {
       statusCode: 200,
